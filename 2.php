c0efd4f8227f0834f1f127d0cfd6698b9407338c
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2017-12-22 14:41:57 +0400

    Update 2.php

--- 2.php
+++ 2.php
@@ -34,13 +34,12 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } 
 
  $email = $_POST['email'];
-    $sql_select = "SELECT email FROM registration_tbl where email = '$email' ";
+    $sql_select = "SELECT email FROM registration_tbl";
   //$sql_select = "Select vopros FROM registration_tbl where email = '$email' ";
 $stmt = $conn->query($sql_select);
-/*$registrants = $stmt->fetchAll();
-       if(count($registrants) > 0) {
+       if( email = '$email') {
   echo "1";
-	      foreach($registrants as $registrant){
+/*	      foreach($registrants as $registrant){
   echo "<p> 123123 </p>";
 }
 }
