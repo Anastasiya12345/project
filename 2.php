50018dc27dd8d27d1c9713115795df9e689fed94
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2017-12-28 11:25:12 +0400

    Update 2.php

--- 2.php
+++ 2.php
@@ -33,7 +33,7 @@ $email1 = $_POST['email1'];
 try { $conn = new PDO("sqlsrv:server = tcp:karl.database.windows.net,1433; Database = db", "Anastasiya", "L4x78tm2p1");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-$sql_select = "SELECT email FROM registration_tbl WHERE email=$email1";
+$sql_select = "SELECT email, vopros FROM registration_tbl WHERE email=$email1";
 $stmt = $conn->query($sql_select);
 $registrants=$stmt->fetchAll();
  foreach($registrants as $registrant){
