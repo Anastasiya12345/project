1c5b530fb7e757abb881865e99c5bc04767afe90
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2017-12-28 11:39:17 +0400

    Update 2.php

--- 2.php
+++ 2.php
@@ -23,17 +23,14 @@ border: 0 none; }
 <body> 
 <h1> Восстановление пароля </h1> 
 <p>Введите Email и нажмите кнопку <strong> Готово </strong>.</p> 
-<form method="post" action="2.php" 
-enctype="multipart/form-data" >
-Email <input type="text" 
-name="email1" id="email1"/></br> 
-<input type = "submit" value = "Проверить" name = "sub" id = "sub">
+<form method="post" action="2.php" enctype="multipart/form-data" >
+Email <input type="text" name="email1" id="email1"/></br> 
 <?php 
 $email1 = $_POST['email1']; 
 try { $conn = new PDO("sqlsrv:server = tcp:karl.database.windows.net,1433; Database = db", "Anastasiya", "L4x78tm2p1");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-$sql_select = "SELECT * FROM registration_tbl WHERE $email=$email1";
+$sql_select = "SELECT email, vopros FROM registration_tbl WHERE email = '$email1'";
 $stmt = $conn->query($sql_select);
 $registrants=$stmt->fetchAll();
 foreach($registrants as $registrant) { 
