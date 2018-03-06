8a297be56bd54780c34f80c1aabe343ed63f59be
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2018-03-06 12:37:57 +0300

    Create oform.php

---
+++ oform.php
@@ -0,0 +1,46 @@
+<html> 
+<head> 
+<Title>Страхование</Title> 
+<style type="text/css"> 
+body { background-color: 
+#fff; border-top: solid 10px #000; 
+color: #333; font-size: .85em; 
+margin: 20; padding: 20; 
+font-family: "Segoe UI", 
+Verdana, Helvetica, Sans-Serif; 
+} 
+h1, h2, h3,{ color: #000; 
+margin-bottom: 0; padding-bottom: 0; } 
+h1 { font-size: 2em; } 
+h2 { font-size: 1.75em; } 
+h3 { font-size: 1.2em; } 
+table { margin-top: 0.75em; } 
+th { font-size: 1.2em; 
+text-align: left; border: none; padding-left: 0; } 
+td { padding: 0.25em 2em 0.25em 0em; 
+border: 0 none; } 
+</style> 
+</head> 
+<body> 
+<h1>Оформление заявки на страховку жизни</h1> 
+<form method="post" action="index.php" 
+enctype="multipart/form-data" > 
+Фамилия <input type="text" 
+name="familiya" id="familiya"/></br> 
+Имя <input type="text" 
+name="imya" id="imya"/></br> 
+Отчество <input type="text" 
+name="otchestvo" id="otchestvo"/></br> 
+Номер паспорта <input type="text" 
+name="nomtel" id="nomtel"/></br> 
+Серия паспорта <input type="text" 
+name="nomtel" id="nomtel"/></br> 
+Номер телефона <input type="text" 
+name="nomtel" id="nomtel"/></br> 
+Пароль <input type="text" 
+name="password" id="password"/></br>
+<input type="submit" 
+name="submit" value="Регестрация"
+</form> 
+</body> 
+</html>
