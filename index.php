a6d50524d5bb95b3a26cb84b841cb6442c8b936a
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2018-03-31 20:41:45 +0400

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ name="password" id="password"/></br>
 name="submit" value="Регестрация"/></br>
 <a href="https://anastasiya.azurewebsites.net/vhod.php">Вход</a></br>
 <?php
-try { $conn = new PDO("sqlsrv:server = tcp:karl.database.windows.net,1433; Database = db", "Anastasiya", "L4x78tm2p1");
+try { $conn = new PDO("sqlsrv:server = tcp:karl.database.windows.net,1433; Database = basa", "Anastasiya", "L4x78tm2p1");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -49,7 +49,7 @@ $password = $_POST['password'];
 // Insert data
 //Запись в БД
    $sql_insert =
-"INSERT INTO registration_tbl1 (nomtel, password)
+"INSERT INTO registration_tbl (tel, password)
 VALUES (?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $tel);
@@ -63,7 +63,7 @@ die(var_dump($e));
    echo "<h3>Your're registered!</h3>"; 
 }
 //Вывод таблицы
-$sql_select = "SELECT * FROM registration_tbl1";
+$sql_select = "SELECT * FROM registration_tbl";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll();
 //Условие. Если количество записей больше 0, тогда выводится записи полей. В противном случае выводится ошибка о регестрации.
