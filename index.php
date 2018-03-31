8a96f7449323f8b4ff99b53ddcf73629b62d3e15
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2018-03-31 20:43:33 +0400

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $password = $_POST['password'];
 // Insert data
 //Запись в БД
    $sql_insert =
-"INSERT INTO registration_tbl (tel, password)
+"INSERT INTO registration_tbl1 (tel, password)
 VALUES (?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $tel);
@@ -63,7 +63,7 @@ die(var_dump($e));
    echo "<h3>Your're registered!</h3>"; 
 }
 //Вывод таблицы
-$sql_select = "SELECT * FROM registration_tbl";
+$sql_select = "SELECT * FROM registration_tbl1";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll();
 //Условие. Если количество записей больше 0, тогда выводится записи полей. В противном случае выводится ошибка о регестрации.
