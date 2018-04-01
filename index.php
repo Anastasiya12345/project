54480a7f56a7052aac456396b8b8d9165c7c2427
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2018-04-01 13:50:48 +0400

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ try { $conn = new PDO("sqlsrv:server = tcp:karl.database.windows.net,1433; Datab
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
-print("Error connecting to SQL Server.");
+print("Ошибка подключения к SQL Server.");
 die(print_r($e));
 }
 //Проверка заполнения при ножатии кнопки. Если поля пустые ничего в БД не записывается.
@@ -49,7 +49,7 @@ $password = $_POST['password'];
 // Insert data
 //Запись в БД
    $sql_insert =
-"INSERT INTO registration_tbl1 (tel, password)
+"INSERT INTO registration_tbl (tel, password)
 VALUES (?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $tel);
@@ -63,7 +63,7 @@ die(var_dump($e));
    echo "<h3>Your're registered!</h3>"; 
 }
 //Вывод таблицы
-$sql_select = "SELECT * FROM registration_tbl1";
+$sql_select = "SELECT * FROM registration_tbl";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll();
 //Условие. Если количество записей больше 0, тогда выводится записи полей. В противном случае выводится ошибка о регестрации.
