0a81fa3cafd07c5cb3774f143443e3be01bc6adf
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2018-04-09 17:44:23 +0400

    Update rasschet.php

--- rasschet.php
+++ rasschet.php
@@ -1,26 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Название документа</title>
-    <style>
-      .okno {
-        width: 300px;
-        height: 50px;
-        text-align: center;
-        padding: 15px;
-        border: 3px solid #0000cc;
-        border-radius: 10px;
-        color: #0000cc;
-      }
-    </style>
-  </head>
- 
-  <body>
-   
-    <div class="okno">
-      Всплывающее окошко!
-    </div>
- 
-  </body>
+    <div id="envelope" class="envelope">
+   <a class="close-btn" href="javascript:void(0)" onclick = "document.getElementById('envelope').style.display='none';document.getElementById('fade').style.display='none'">Закрыть</a>
+     <h1 class="title">Отправить нам сообщение</h1>
+      <form method="post" action="mail/sendletter.php">
+	<input type="text" name="sender" onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'* Ваше Имя':this.value;" value="* Ваше Имя" class="your-name"/>
+	<input type="text" name="email" onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'* Ваш Email':this.value;" value="* Ваш Email" class="email-address"/>
+	<textarea class="your-message">Пожалуйста, введите своё сообщение здесь ..</textarea>
+	<input type="submit" name="send" value="Отправить" class="send-message">
+      </form>
+</div>
+   </body>
 </html>
