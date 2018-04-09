095555de81007b6e6593b77ca8162772b2db96f1
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2018-04-09 10:07:02 +0400

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ border: 0 none; }
 <body> 
 <h1>Оформление заявки</h1> 
 <p>Введите свои данные и нажмите <strong> "Далее" </strong> .</p>
-<form method="post" action="oform.php" 
+<form method="post" action="oform.php"> 
 enctype="multipart/form-data" ></br> 
 Фамилия <input type="text" 
 name="familiya" id="familiya"/></br> 
