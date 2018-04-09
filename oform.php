00428fe60617809735450446b68364fbff1dcce3
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2018-04-09 17:51:58 +0400

    Update oform.php

--- oform.php
+++ oform.php
@@ -23,16 +23,7 @@ border: 0 none; }
              </style>
   </head>
  <h1>Оформление страхования жизни</h1>
-<div id="envelope" class="envelope">
-   <a class="close-btn" href="javascript:void(0)" onclick = "document.getElementById('envelope').style.display='none';document.getElementById('fade').style.display='none'">Закрыть</a>
-     <h1 class="title">Отправить нам сообщение</h1>
-      <form method="post" action="mail/sendletter.php">
-	<input type="text" name="sender" onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'* Ваше Имя':this.value;" value="* Ваше Имя" class="your-name"/>
-	<input type="text" name="email" onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'* Ваш Email':this.value;" value="* Ваш Email" class="email-address"/>
-	<textarea class="your-message">Пожалуйста, введите своё сообщение здесь ..</textarea>
-	<input type="submit" name="send" value="Отправить" class="send-message">
-      </form>
-</div>
+	<a><href=https://anastasiya.azurewebsites.net/rasschet.php><b>Рассчет стоимости ежегодного платежа</b></a>
  <p><i>Заполните анкету.</i></p>
 <form method="post" action="oform.php">
 Серия и номер паспорта <input type="text" 
