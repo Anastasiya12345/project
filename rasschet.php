cbda791a7ccaec38163181d159020ca15f415fd8
Anastasiya12345 <31986524+Anastasiya12345@users.noreply.github.com>
2018-04-09 19:04:10 +0400

    Update rasschet.php

--- rasschet.php
+++ rasschet.php
@@ -1,15 +1,51 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <div id="envelope" class="envelope">
-   <a class="close-btn" href="javascript:void(0)" onclick = "document.getElementById('envelope').style.display='none';document.getElementById('fade').style.display='none'">Закрыть</a>
-     <h1 class="title">Отправить нам сообщение</h1>
-      <form method="post" action="mail/sendletter.php">
-	<input type="text" name="sender" onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'* Ваше Имя':this.value;" value="* Ваше Имя" class="your-name"/>
-	<input type="text" name="email" onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'* Ваш Email':this.value;" value="* Ваш Email" class="email-address"/>
-	<textarea class="your-message">Пожалуйста, введите своё сообщение здесь ..</textarea>
-	<input type="submit" name="send" value="Отправить" class="send-message">
-      </form>
-</div>
-   </body>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8"><Title>Страхование</Title>
+<style type="text/css">
+    body { background-color:
+ #fff; border-top: solid 10px #000;
+ color: #333; font-size: .85em;
+ margin: 20; padding: 20;
+ font-family: "Segoe UI",
+ Verdana, Helvetica, Sans-Serif;
+    }
+    h1, h2, h3,{ color: #000;
+margin-bottom: 0; padding-bottom: 0; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
+    table { margin-top: 0.75em; }
+    th { font-size: 1.2em;
+ text-align: left; border: none; padding-left: 0; }
+    td { padding: 0.25em 2em 0.25em 0em;
+border: 0 none; }
+             </style>
+  </head>
+ <h1>Оформление страхования жизни</h1>
+ <p><i>Заполните анкету.</i></p>
+<form method="post" action="oform.php">
+Срок страховой службы <select name="srok">
+<option value ="1">1 год</option>
+<option value ="5">5 лет</option>
+<option value ="10">10 лет</option>
+</select></br>
+Сумма страховки <select name="sum">
+name="pasport" id="pasport"/></br>
+<option value ="100000">100000</option>
+<option value ="500000">500000</option>
+</select></br>
+
+<input type="submit" 
+name="submit" value="Рассчитать"></br>
+</form>
+</body>
 </html>
+<?php
+if (isset($_GET['submit'])){
+    $a = (int) $_GET['srok'];
+    $b = (int) $_GET['sum'];
+
+echo "($a * $b)/100";
+
+?>
